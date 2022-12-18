b96bd3714e473f57126ac2733e3c09d95d8714b7
kenzy1093 <kensworthn@gmail.com>
2022-12-17 19:35:53 -0500

    (module4solution)

--- module4-solution/easier/script.js
+++ module4-solution/easier/script.js
@@ -34,7 +34,6 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
-
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
